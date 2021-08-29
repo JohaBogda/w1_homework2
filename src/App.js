0c1a1c7e2b7af6e642559fe5de0d321c5a2da8af
JohaBogda <bogdanowj@gmail.com>
2021-08-29 08:43:05 -0400

    Grocery List + add new item field

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import groceries from './groceries';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+  state = {
+    // 
+    groceries:groceries,
+    // default values
+    item: "",
+    quantity: "",
+    units: "",
+  };
+
+  handleChange = (event) => {
+    // console.log(event.target.value)
+    this.setState({
+      // item: event.target.value,
+      // quantity: event.target.value,
+      // units: event.target.value
+      // to avoid only changing one object in the state, focusing on id gives all objects the opportunity to type into the input
+      [event.target.id]:event.target.value
+    })
+  }
+
+  // arrow function for submit button to add newItem to groceries
+  handleSubmit = (event) => {
+    // preventDefault stops website from refreshing
+    event.preventDefault()
+    const newItem = {
+      item: this.state.item,
+      quantity: this.state.quantity,
+      units: this.state.units
+    }
+    // adding new item to state.groceries: 
+    this.setState({
+      groceries: [newItem, ...this.state.groceries]
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Grocery List</h1>
+        <div>{this.state.groceries.map((parameter) => {
+          return (
+            <li> Item: {parameter.item}; Quantity: {parameter.quantity}; Units: {parameter.units}</li>
+          )
+        })}
+        </div>
+        <div>
+          <h1>Add new item to Grocery List</h1>
+          {/* forms should always come with label to make it look nice/ accessibility and input to type text into */}
+          <form onSubmit={this.handleSubmit}>
+            {/* id connects the event to state */}
+            <label htmlFor="item">Item: </label>
+            <input type="text" id="item" value={this.state.item} onChange={this.handleChange} />
+              <br/>
+            <label htmlFor="quantity">Quantity: </label>
+            <input type="text" id="quantity" value={this.state.quantity} onChange={this.handleChange} />
+              <br/>
+            <label htmlFor="units">Units: </label>
+            <input type="text" id="units" value={this.state.units} onChange={this.handleChange} />
+              <br/>
+            {/* input with submit type becomes a button to submit */}
+            <input type="submit"/>
+          </form>
+        </div>
+        
+      </div>
+    );
+  }
+}
---
+++ src/groceries.jsx
@@ -0,0 +1,27 @@
+const groceries = [
+    {
+        item: 'Nutella',
+        brand: "Ferrero",
+        units: '33 oz',
+        quantity: 0,
+        isPurchased: false
+      },
+
+      {
+        item: 'Banana',
+        brand: 'Chiquita',
+        units: '1lb',
+        quantity: 0,
+        isPurchased: false
+      },
+
+      {
+        item: 'Beer',
+        brand: 'Delirium',
+        units: '1 pt',
+        quantity: 0,
+        isPurchased: false
+      }
+]
+
+export default groceries
